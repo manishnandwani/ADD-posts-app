f8569e3b7f0409f5e71db35e7eaf3dc7f76a28bc
Manish Nandwani <34483364+manishnandwani@users.noreply.github.com>
2019-12-19 12:12:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var bodyParser = require('body-parser');
 var mongodb = require("mongodb");
 var MongoClient = mongodb.MongoClient;
 
-// server.use(express.static('../public/view'));
 server.use(express.static( "public"));
 server.use(bodyParser.json())
 
@@ -18,9 +17,7 @@ MongoClient.connect(url,function(err,client){
 })
 
 server.get("/list",function(req,res){
-    console.log("test")
     db.collection("posts").find({}).toArray(function(err,results){
-        console.log("test",results)
         res.json(results)
     },function(err){
         res.send('results.')
@@ -28,9 +25,7 @@ server.get("/list",function(req,res){
 })
 
 server.post("/add",function(req,res){
-    console.log("body",req.body)
     db.collection("posts").insertOne(req.body,function(err,results){
-        // console.log("lets0---",results,err)
         if(err == null){
             res.json(req.body)
         }
@@ -38,11 +33,8 @@ server.post("/add",function(req,res){
 })
 
 server.put("/edit",function(req,res){
-    console.log("body",req.body)
     db.collection("posts").updateOne({_id :new mongodb.ObjectID( req.body._id)},{$set:{votes :req.body.votes}},function(err,results){
-        // console.log("lets0---",results,err)
         if(err == null){
-            // console.log("edit",results)
             res.json(req.body)
         }
         else{
