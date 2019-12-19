be1db56f4ae0c4edf5b89912b7158faad5ac7a30
Manish Nandwani <34483364+manishnandwani@users.noreply.github.com>
2019-12-19 17:46:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
-var express=require('express');
+var express = require('express');
 var server = express()
 var bodyParser = require('body-parser');
 
-var mongodb = require("mongodb");
-var MongoClient = mongodb.MongoClient;
+// var mongodb = require("mongodb");
+// var MongoClient = mongodb.MongoClient;
+var mongoose = require('mongoose');
 
-server.use(express.static( "public"));
+server.use(express.static("public"));
 server.use(bodyParser.json())
 
-var url = "mongodb://54.89.215.71:27017";
-var db;
+var url = "mongodb://localhost:27017";
+var _db;
+mongoose.connect(url, function (err, db) {
+    if (err)
+        console.log(err)
+    _db = db;
+    console.log('Connection established to');
+});
 
-MongoClient.connect(url,{
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-},function(err,client){
-    if(err){
-        console.log("error :",err);
-    }else{    
-        db = client.db("demoDB");
-    }
-})
+// MongoClient.connect(url, function (err, client) {
+//     db = client.db("demoDB");
+//     console.log("error :", err);
+// })
 
-server.get("/list",function(req,res){
-    db.collection("posts").find({}).toArray(function(err,results){
+server.get("/list", function (req, res) {
+    _db.collection("posts").find({}).toArray(function (err, results) {
         res.json(results)
-    },function(err){
+    }, function (err) {
         res.send('results.')
     })
 })
 
-server.post("/add",function(req,res){
-    db.collection("posts").insertOne(req.body,function(err,results){
-        if(err == null){
+server.post("/add", function (req, res) {
+    _db.collection("posts").insertOne(req.body, function (err, results) {
+        if (err == null) {
             res.json(req.body)
         }
     })
 })
 
-server.put("/edit",function(req,res){
-    db.collection("posts").updateOne({_id :new mongodb.ObjectID( req.body._id)},{$set:{votes :req.body.votes}},function(err,results){
-        if(err == null){
-            res.json(req.body)
+server.put("/edit", function (req, res) {
+    _db.collection("posts").updateOne({
+        _id: new mongodb.ObjectID(req.body._id)
+    }, {
+        $set: {
+            votes: req.body.votes
         }
-        else{
+    }, function (err, results) {
+        if (err == null) {
+            res.json(req.body)
+        } else {
             console.log(err)
         }
     })
 })
 
-server.listen(8000,function(){
+server.listen(8000, function () {
     console.log("server started")
 })
