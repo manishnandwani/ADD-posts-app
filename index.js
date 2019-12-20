552b205f3ff698c7019ab1a007e530d74a9c100d
Manish Nandwani <34483364+manishnandwani@users.noreply.github.com>
2019-12-20 11:28:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ server.post("/add", function (req, res) {
 
 server.put("/edit", function (req, res) {
     _db.collection("posts").updateOne({
-        _id: ObjectId(req.body._id)
+        _id: mongoose.Types.ObjectId(req.body._id)
     }, {
         $set: {
             votes: req.body.votes
